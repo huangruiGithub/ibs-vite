126da2bf00e79be90970b25883e7e394726bcd5b
zhanghan <zh2314560911@163.com>
2023-02-28 11:04:19 +0800

    feat: 管理菜单管理页

---
+++ src/views/system-management/components/menu-cpns/menuTree.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="menu-tree">
+    <!-- 搜索输入框 -->
+    <el-input v-model="serachVal" class="search-input" placeholder="输入关键字进行过滤">
+      <template #prefix>
+        <el-icon class="el-input__icon"><search /></el-icon>
+      </template>
+    </el-input>
+
+    <el-tree
+      ref="treeRef"
+      class="filter-tree"
+      :data="data"
+      :props="defaultProps"
+      default-expand-all
+      :filter-node-method="filterNode"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import { ElTree } from 'element-plus'
+
+interface Tree {
+  id: number
+  label: string
+  children?: Tree[]
+}
+
+const serachVal = ref('')
+
+const treeRef = ref<InstanceType<typeof ElTree>>()
+
+const defaultProps = {
+  children: 'children',
+  label: 'label'
+}
+
+watch(serachVal, (val) => {
+  treeRef.value!.filter(val)
+})
+
+const filterNode = (value: string, data: Tree) => {
+  if (!value) return true
+  return data.label.includes(value)
+}
+
+const data: Tree[] = [
+  {
+    id: 1,
+    label: 'Level one 1',
+    children: [
+      {
+        id: 4,
+        label: 'Level two 1-1',
+        children: [
+          {
+            id: 9,
+            label: 'Level three 1-1-1'
+          },
+          {
+            id: 10,
+            label: 'Level three 1-1-2'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    label: 'Level one 2',
+    children: [
+      {
+        id: 5,
+        label: 'Level two 2-1'
+      },
+      {
+        id: 6,
+        label: 'Level two 2-2'
+      }
+    ]
+  },
+  {
+    id: 3,
+    label: 'Level one 3',
+    children: [
+      {
+        id: 7,
+        label: 'Level two 3-1'
+      },
+      {
+        id: 8,
+        label: 'Level two 3-2'
+      }
+    ]
+  }
+]
+</script>
+<style lang="less" scoped>
+.menu-tree {
+  .search-input {
+    height: 32px;
+  }
+  .filter-tree {
+    margin-top: 16px;
+  }
+}
+</style>
---
+++ src/views/system-management/menu.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="sys-menu">
+    <div class="left comm-border">
+      <div class="left-hd">
+        <div class="comm-title">
+          <span class="line"></span>
+          <span class="title">菜单树</span>
+        </div>
+      </div>
+      <!-- 内容 -->
+      <div class="left-bd">
+        <menuTree />
+      </div>
+    </div>
+    <div class="right comm-border">
+      <div class="right-bd">
+        <div class="comm-title">
+          <span class="line"></span>
+          <span class="title">菜单架构图</span>
+          <div class="btns"><el-button size="small">下载</el-button></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import menuTree from './components/menu-cpns/menuTree.vue'
+</script>
+
+<style lang="less" scoped>
+.sys-menu {
+  display: flex;
+  flex-shrink: 0;
+  margin: 8px;
+  .left {
+    width: 300px;
+    height: calc(100vh - 64px);
+    margin-right: 8px;
+    padding: 16px;
+    box-sizing: border-box;
+    .left-hd {
+      padding-bottom: 8px;
+      border-bottom: 1px solid #dcdfe6;
+    }
+    .left-bd {
+      margin-top: 8px;
+    }
+  }
+  .right {
+    flex: 1;
+    margin-left: 8px;
+    padding: 16px;
+    box-sizing: border-box;
+    .right-bd {
+      padding-bottom: 8px;
+      border-bottom: 1px solid #dcdfe6;
+      .btns {
+        margin-left: 24px;
+      }
+    }
+  }
+
+  .comm-border {
+    box-sizing: border-box;
+    box-shadow: 0 0 6px rgba(0, 0, 0, 0.12);
+  }
+  .comm-title {
+    display: flex;
+    align-content: center;
+    .line::after {
+      content: '';
+      display: inline-block;
+      width: 2px;
+      height: 100%;
+      margin-right: 16px;
+      background-color: var(--primary-color);
+    }
+    .title {
+      font-weight: 600;
+    }
+  }
+}
+</style>
